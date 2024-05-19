b4ba3223fce104154add57155abae46754aa9f68
r7kamura <r7kamura@gmail.com>
2024-05-20 07:39:35 +0900

    Add support for automaton-media.com

--- src/patterns.ts
+++ src/patterns.ts
@@ -6,6 +6,10 @@ export type Pattern = {
 // Ordered by url in alphabetical order.
 export const patterns: Pattern[] = [
   {
+    url: "^https://automaton-media.com/$",
+    selector: ".td-pb-span8 .td-block-span12 .entry-title a",
+  },
+  {
     url: "^https://www.amazon.co.jp/s\\?",
     selector: "h2 a",
   },
